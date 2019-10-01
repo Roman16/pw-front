dac1cd1d2ac58953112e171356e2db5af453ce96
Romaniv Vitalii <romanivvit@gmail.com>
2019-10-01 22:10:17 +0300

    Add new interceptor and error display with loading
    Signed-off-by: Romaniv Vitalii <romanivvit@gmail.com>

--- src/authentication/interceptors.js
+++ src/authentication/interceptors.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
 window.BASE_URL = process.env.NODE_ENV === 'production' ? '' : 'http://front1.profitwhales.com';
+const token = global.localStorage.getItem('token');
 
 const initialInterceptors = () => {
     axios.interceptors.request.use(
         (config) => {
             config.baseURL = window.BASE_URL;
-
+            config.headers.Authorization = token;
 
             return config;
         },
