7868579a6b7c190d26eee070a261c71ec0f3a575
Kh-Ol-An <olegkhristenko@gmail.com>
2019-10-17 11:07:47 +0300

    ssh try 2

--- src/pages/PPCAutomate/Report/Report.js
+++ src/pages/PPCAutomate/Report/Report.js
@@ -3,7 +3,6 @@ import ProductList from '../../../components/ProductList/ProductList';
 import ReportTable from './ReportTable/ReportTable';
 import './Report.less';
 
-// delete this coment
 function Report() {
     return (
         <div className="ProductMain basic-container">
