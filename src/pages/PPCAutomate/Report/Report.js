251eb42f380fb454b30a714a57706f07dc837dd3
Kh-Ol-An <olegkhristenko@gmail.com>
2019-10-31 19:20:42 +0200

    fix

--- src/pages/PPCAutomate/Report/Report.js
+++ src/pages/PPCAutomate/Report/Report.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ProductList from '../../../components/ProductList/ProductList';
 import ReportTable from './ReportTable/ReportTable';
 
-import './Report.less';
-
 function Report() {
     return (
         <div className="product-main basic-container reports-page">
-            <ProductList/>
-            <ReportTable/>
+            <ProductList />
+            <ReportTable />
         </div>
     );
 }
