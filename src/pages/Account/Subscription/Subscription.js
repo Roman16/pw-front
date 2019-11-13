bedc660985d5bc3ef621a097ff6d162f4a3d346f
Kh-Ol-An <olegkhristenko@gmail.com>
2019-11-13 18:04:27 +0200

    fix

--- src/pages/Account/Subscription/Subscription.js
+++ src/pages/Account/Subscription/Subscription.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 import Navigation from "../Navigation/Navigation";
 import reload from "../../../assets/img/icons/reload.svg";
+import "./Subscription.less";
 
 const Subscription = () => {
   return (
@@ -10,44 +11,55 @@ const Subscription = () => {
       <Navigation />
       <div className="automate-box">
         <div className="reactivate">
-          <h3>Your Subscription Has Been Cancelled</h3>
-          <p>
+          <h3 className="reactivate-title">
+            Your Subscription Has Been Cancelled
+          </h3>
+          <p className="reactivate-text">
             You will have access to the software untill the end of this billin
-            cycle <span>(August 14, 2019)</span>
+            cycle (<span className="reactivate-data">August 14, 2019</span>)
           </p>
-          <button type="button">
-            Reactivate
-            <img src={reload} alt="reload" />
+          <button className="reactivate-btn" type="button">
+            <span>Reactivate</span>
+            <img className="reactivate-img" src={reload} alt="reload" />
           </button>
         </div>
-        <div>
-          <div className="automate">
-            <h2>PPC Automate</h2>
-            <p>Acces and edit your accounts Profit Whales subscription</p>
-            <Link to="#">To view your invoices, see billing info</Link>
+        <div className="automate">
+          <div className="ppc">
+            <h2 className="ppc-title">PPC Automate</h2>
+            <p className="ppc-text">
+              Acces and edit your accounts Profit Whales subscription
+            </p>
+            <Link className="ppc-link" to="/account/billing">
+              To view your invoices, see billing info
+            </Link>
           </div>
-          <div className="charged-box">
-            <div className="charged-wrap">
-              <h3>Subscription Plan</h3>
-              <p>
-                You’ll be charged <span>$ 347</span>
+          <div className="plan">
+            <div className="charged">
+              <h3 className="charged-title">Subscription Plan</h3>
+              <p className="charged-text">
+                You’ll be charged <span className="charged-data">$ 347</span>
               </p>
-              <div className="indicators-wrap">
-                <p>
-                  based on <span>$ 145 + 4%</span>
+              <div className="indicators">
+                <p className="indicators-text">
+                  based on <span className="indicators-data">$ 145 + 4%</span>
                 </p>
-                <Link to="#">monthly ad spend</Link>
+                <Link className="indicators-link" to="#">
+                  monthly ad spend
+                </Link>
               </div>
             </div>
-            <p>
-              Your Ad Spend: <span>$ 1,347</span>
+            <p className="plan-text">
+              Your Ad Spend: <span className="plan-data">$ 1,347</span>
             </p>
-            <div className="cancel-box">
-              <p>
-                Next Invoice Date: <span>May 14, 2019</span>
-              </p>
-              <button type="button">Cancel</button>
-            </div>
+          </div>
+          <div className="cancel">
+            <p className="cancel-text">
+              Next Invoice Date:
+              <span className="cancel-data">May 14, 2019</span>
+            </p>
+            <button className="cancel-btn" type="button">
+              Cancel
+            </button>
           </div>
         </div>
       </div>
