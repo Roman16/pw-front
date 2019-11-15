6c21583a0b55dc45f30a81ebd81eae2c620b8328
Kh-Ol-An <olegkhristenko@gmail.com>
2019-11-15 15:31:39 +0200

    modal sub end

---
+++ src/pages/Account/Subscription/DrawerWindows/CancelAccountWindow.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import cancel from '../../../../assets/img/cancel.svg';
+import safe from '../../../../assets/img/safe.svg';
+import bag from '../../../../assets/img/bag.svg';
+import restart from '../../../../assets/img/restart.svg';
+import square from '../../../../assets/img/icons/green-square.svg';
+
+const CancelAccountWindow = ({ onCloseWindow }) => {
+  return (
+    <div className="cancel">
+      <button className="close-btn" type="button" onClick={onCloseWindow}>
+        &#215;
+      </button>
+      <div className="cancel-wrap">
+        <h3 className="title">Cancel Your Account</h3>
+        <img className="cancel-logo" src={cancel} alt="cancel" />
+        <h2 className="main-title">
+          Did you know that you can pause your ProfitWhales subscription?
+        </h2>
+        <p className="main-text">
+          <span>Don't lose all of your work!</span> When you pause your
+          ProfitWhales account you will still have access to all your
+          Advertising Data Points
+        </p>
+        <p className="text">
+          Pause your account for a lowered price to
+          <span>$19.99/month</span>
+        </p>
+        <ul className="advantages-list">
+          <li className="advantages">
+            <img className="advantages-img" src={safe} alt="safe" />
+            <p className="advantages-text">
+              Have access to the dashboard and advertising change history
+            </p>
+          </li>
+          <li className="advantages">
+            <img className="advantages-img" src={bag} alt="bag" />
+            <p className="advantages-text">Don’t lose the valuable data</p>
+          </li>
+          <li className="advantages">
+            <img className="restart-img" src={restart} alt="restart" />
+            <p className="advantages-text">
+              One click to renuew your subscription
+            </p>
+          </li>
+        </ul>
+      </div>
+      <div className="btn-wrapper">
+        <div className="btn-wrap">
+          <button className="keep" type="button">
+            Keep my account
+          </button>
+          <button className="pause-btn" type="button">
+            <img className="pause-img" src={square} alt="square" />
+            Yes, pause my subscription
+          </button>
+        </div>
+        <button className="cancel-btn" type="button">
+          I still want to cancel
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CancelAccountWindow;
--- src/pages/Account/Subscription/Subscription.js
+++ src/pages/Account/Subscription/Subscription.js
@@ -1,77 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Drawer } from 'antd';
 
 import Navigation from '../Navigation/Navigation';
-import reload from '../../../assets/img/icons/reload.svg';
-import ppcIcon from '../../../assets/img/icons/ppc-automate-icon.svg';
+import PPCAutomate from './PPCAutomate';
+import CancelAccountWindow from './DrawerWindows/CancelAccountWindow';
 import './Subscription.less';
+import './DrawerWindows/CancelAccountWindow.less';
 
 const Subscription = () => {
+  const [openedWindow, openWindow] = useState(null);
+
+  function handleOpenWindow(window) {
+    openWindow(window);
+  }
+
+  function handleCloseWindow() {
+    openWindow(null);
+  }
+
   return (
     <div className="user-cabinet">
       <Navigation />
-      <div className="automate-box">
-        <div className="reactivate">
-          <h3 className="reactivate-title">
-            Your Subscription Has Been Cancelled
-          </h3>
-          <p className="reactivate-text">
-            You will have access to the software untill the end of this billin
-            cycle (<span className="reactivate-data">August 14, 2019</span>)
-          </p>
-          <button className="reactivate-btn" type="button">
-            Reactivate
-            <img className="reactivate-img" src={reload} alt="reload" />
-          </button>
-        </div>
-        <div className="automate">
-          <div className="ppc">
-            <div className="ppc-title-wrap">
-              <img className="ppc-icon" src={ppcIcon} alt="icon" />
-              <h2 className="ppc-title">PPC Automate</h2>
-            </div>
-            <p className="ppc-text">
-              Acces and edit your accounts Profit Whales subscription
-            </p>
-            <Link className="ppc-link" to="/account/billing">
-              To view your invoices, see billing info
-            </Link>
-          </div>
-          <div className="plan">
-            <div className="charged">
-              <div className="charged-wrap">
-                <h3 className="charged-title">
-                  Subscription <br /> Plan
-                </h3>
-                <div className="charged-description">
-                  <p className="charged-text">You’ll be charged</p>
-                  <p className="charged-data">$ 347</p>
-                </div>
-              </div>
-              <div className="indicators">
-                <p className="indicators-text">
-                  based on <span className="indicators-data">$ 145 + 4%</span>
-                </p>
-                <Link className="indicators-link" to="#">
-                  monthly ad spend
-                </Link>
-              </div>
-            </div>
-            <p className="plan-text">
-              Your Ad Spend: <span className="plan-data">$ 1,347</span>
-            </p>
-          </div>
-          <div className="cancel">
-            <p className="cancel-text">
-              Next Invoice Date:
-              <span className="cancel-data">May 14, 2019</span>
-            </p>
-            <button className="cancel-btn" type="button">
-              Cancel
-            </button>
-          </div>
-        </div>
-      </div>
+
+      <PPCAutomate onOpenWindow={handleOpenWindow} />
+
+      <Drawer
+        placement="right"
+        className="cancel-account"
+        closable={false}
+        onClose={() => openWindow(null)}
+        visible={openedWindow}
+      >
+        <CancelAccountWindow onCloseWindow={handleCloseWindow} />
+      </Drawer>
     </div>
   );
 };
