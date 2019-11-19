687a3293a0bcdb587031abdd3d6781ea599bc693
Roman16 <roman.kostyanko@gmail.com>
2019-11-19 16:49:50 +0200

    changed metrics key

--- src/pages/PPCAutomate/Dashboard/Metrics/metricsList.js
+++ src/pages/PPCAutomate/Dashboard/Metrics/metricsList.js
@@ -38,7 +38,7 @@ export const metricsListArray = [
     },
     {
         title: 'Orders',
-        key: 'orders',
+        key: 'total_orders',
         info: '',
         label: 'Total',
         type: ''
@@ -108,127 +108,9 @@ export const metricsListArray = [
     },
     {
         title: 'MACoS',
-        key: 'MACoS',
+        key: 'macos',
         info: '',
         label: 'Average',
         type: 'percent'
     }
 ];
-
-export const metricsListObject = {
-    'impressions': {
-        title: 'Impressions',
-        key: 'impressions',
-        info: '',
-        label: 'Total',
-        type: ''
-    },
-    'clicks': {
-        title: 'Clicks',
-        key: 'clicks',
-        info: '',
-        label: 'Total',
-        type: '',
-        active: true
-    },
-    'ctr': {
-        title: 'CTR',
-        key: 'ctr',
-        info: '',
-        label: 'Average',
-        type: 'percent'
-    },
-    'spend': {
-        title: 'Spend',
-        key: 'spend',
-        info: '',
-        label: 'Total',
-        type: 'currency'
-    },
-    'cpc': {
-        title: 'CPC',
-        key: 'cpc',
-        info: '',
-        label: 'Average',
-        type: 'currency',
-        active: true
-    },
-    'orders': {
-        title: 'Orders',
-        key: 'orders',
-        info: '',
-        label: 'Total',
-        type: ''
-    },
-    'ad_orders': {
-        title: 'Ad Orders',
-        key: 'ad_orders',
-        info: '',
-        label: 'Total',
-        type: ''
-    },
-    'organic_orders': {
-        title: 'Organic Orders',
-        key: 'organic_orders',
-        info: '',
-        label: 'Total',
-        type: ''
-    },
-    'total_sales': {
-        title: 'Total Sales',
-        key: 'total_sales',
-        info: '',
-        label: 'Total',
-        type: 'currency'
-    },
-    'ad_sales': {
-        title: 'Ad Sales',
-        key: 'ad_sales',
-        info: '',
-        label: 'Total',
-        type: 'currency'
-    },
-    'organic_sales': {
-        title: 'Organic Sales',
-        key: 'organic_sales',
-        info: '',
-        label: 'Total',
-        type: 'currency'
-    },
-    'acos': {
-        title: 'ACoS',
-        key: 'acos',
-        info: '',
-        label: 'Average',
-        type: ''
-    },
-    'conversion_rate': {
-        title: 'Conversion Rate',
-        key: 'conversion_rate',
-        info: '',
-        label: 'Average',
-        type: ''
-    },
-    'cpa': {
-        title: 'CPA',
-        key: 'cpa',
-        info: '',
-        label: 'Average',
-        type: 'currency'
-    },
-    'profit': {
-        title: 'Profit',
-        key: 'profit',
-        info: '',
-        label: 'Total',
-        type: 'currency'
-    },
-    'MACoS': {
-        title: 'MACoS',
-        key: 'MACoS',
-        info: '',
-        label: 'Average',
-        type: 'percent'
-    }
-};
-
