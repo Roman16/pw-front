fcab6aff2aa6a23074246d78fa51023a61b40642
Kh-Ol-An <olegkhristenko@gmail.com>
2019-11-21 13:03:37 +0200

    reports

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,35 +1,36 @@
 import React from 'react';
-import {Table as AntTable} from 'antd';
-import './Table.less';
+import { Table as AntTable } from 'antd';
+// import './Table.less';
 
-const Table = (props) => {
-    const {
-        pageSize = 10,
-        totalSize = 0,
-        onChangePagination,
-        currentPage,
-        showPagination = false,
-        scroll
-    } = props;
+const Table = props => {
+  const {
+    pageSize = 10,
+    totalSize = 0,
+    onChangePagination,
+    currentPage,
+    showPagination = false,
+    scroll
+  } = props;
 
-    const paginationSettings = showPagination && (totalSize > pageSize)
-        ? {
-            total: totalSize,
-            current: currentPage,
-            pageSize: pageSize,
-            onChange: onChangePagination,
+  const paginationSettings =
+    showPagination && totalSize > pageSize
+      ? {
+          total: totalSize,
+          current: currentPage,
+          pageSize: pageSize,
+          onChange: onChangePagination
         }
-        : false;
+      : false;
 
-    return (
-        <div className="Table">
-            <AntTable
-                {...props}
-                scroll={{ y: showPagination ? (scroll - 80) : scroll}}
-                pagination={paginationSettings}
-            />
-        </div>
-    );
+  return (
+    <div className="Table">
+      <AntTable
+        {...props}
+        scroll={{ y: showPagination ? scroll - 80 : scroll }}
+        pagination={paginationSettings}
+      />
+    </div>
+  );
 };
 
 export default Table;
