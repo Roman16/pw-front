e4f041236a3db4e424e0dfa8c05bf797a615bdf7
Kh-Ol-An <olegkhristenko@gmail.com>
2019-11-26 14:19:09 +0200

    fix avatar in sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -220,7 +220,6 @@ const Sidebar = () => {
                                       <Avatar
                                           className="avatar"
                                           src={domainName + user.user.avatar}
-                                          size={30}
                                       />
                                   </i>
                               ) : (
