a4c365078bebce8f3aff7fb2e95ec57ab71715b1
Roman16 <roman.kostyanko@gmail.com>
2019-11-29 13:27:11 +0200

    added home link to sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -84,7 +84,7 @@ const Sidebar = () => {
         <div className={`sidebar ${className}`}>
             <div className="sidebar-header">
                 <Icon className="sidebar-icon" type="menu" onClick={toggleCollapsed}/>
-                <Link to="/" className="sidebar-logo">
+                <Link to="/home" className="sidebar-logo">
                     <img className="logo" src={logo} alt="logo"/>
                 </Link>
             </div>
