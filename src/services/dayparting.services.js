4da30257864caee6dbbff037568f7abc2435ee00
Roman16 <roman.kostyanko@gmail.com>
2020-03-10 16:56:19 +0200

    changed time zone shift on dayparting

--- src/services/dayparting.services.js
+++ src/services/dayparting.services.js
@@ -48,7 +48,7 @@ function getPlacementsStatistic({campaignId, date, cancelToken}) {
 }
 
 function getDayPartingParams({campaignId, cancelToken}) {
-    return api('get', `${daypartingUrls.dayParting(campaignId)}?size=1&page=1`, null, null, cancelToken)
+    return api('get', `${daypartingUrls.getDayParting(campaignId)}?size=1&page=1`, null, null, cancelToken)
 }
 
 function updateDayPartingParams({campaignId, state_encoded_string, status}) {
