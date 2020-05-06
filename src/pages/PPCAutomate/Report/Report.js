db31277c5e243e6bd3ec366cec2ebc9ea41ef3c3
Romka <romka@MacBook-Pro-1.local>
2020-05-06 14:13:56 +0300

    returned old reports version

--- src/pages/PPCAutomate/Report/Report.js
+++ src/pages/PPCAutomate/Report/Report.js
@@ -4,19 +4,10 @@ import SubscriptionNotificationWindow from "../../../components/ModalWindow/Info
 import LoadingAmazonAccount from "../../../components/ModalWindow/InformationWindows/LoadingAmazonAccountWindow";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Tabs from "./Tabs/Tabs";
-import FreeTrial from "../../../components/FreeTrial/FreeTrial";
-import Filters from "./Filters/Filters";
 
 function Report() {
     return (
         <div className="product-main basic-container reports-page">
-            <FreeTrial product={'ppc'}/>
-
-            <Tabs />
-
-            <Filters/>
-
             <ReportTable />
 
             <SubscriptionNotificationWindow product={'ppc'}/>
