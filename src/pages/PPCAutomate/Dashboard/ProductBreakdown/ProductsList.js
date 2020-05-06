a2295b4055965d7b1c449e890f60daacb1c7b18d
Romka <romka@MacBook-Pro-1.local>
2020-05-06 22:13:59 +0300

    updated product breakdown on dashboard

--- src/pages/PPCAutomate/Dashboard/ProductBreakdown/ProductsList.js
+++ src/pages/PPCAutomate/Dashboard/ProductBreakdown/ProductsList.js
@@ -89,7 +89,11 @@ const ProductsList = ({products, onSearchChange, fetchParams, handlePaginationCh
             )
         },
         {
-            title: 'Budget Allocation',
+            title: () => <div>Budget Allocation
+                <Tooltip
+                    getPopupContainer={trigger => trigger.parentNode}
+                    description={'A budget allocation is the amount of funding designated to each of your product.'}
+                /></div>,
             dataIndex: 'budget_allocation',
             key: 'budget_allocation',
             width: '10em',
@@ -105,10 +109,11 @@ const ProductsList = ({products, onSearchChange, fetchParams, handlePaginationCh
             )
         },
         {
-            title: () => <div>Ad Sales <br/> Share<Tooltip getPopupContainer={trigger => trigger.parentNode}
-
-                                                           description={'The percentage of sales allocated to the given product.'}
-            /></div>,
+            title: () => <div>Ad Sales Share
+                <Tooltip
+                    getPopupContainer={trigger => trigger.parentNode}
+                    description={'The percentage of sales allocated to the given product.'}
+                /></div>,
             dataIndex: 'sales_share',
             key: 'sales_share',
             width: '11em',
@@ -124,9 +129,9 @@ const ProductsList = ({products, onSearchChange, fetchParams, handlePaginationCh
             )
         },
         {
-            title: () => <div>CPA<Tooltip getPopupContainer={trigger => trigger.parentNode}
-
-                                          description={'Cost to acquire one paying customer on a campaign.'}
+            title: () => <div>CPA<Tooltip
+                getPopupContainer={trigger => trigger.parentNode}
+                description={'Cost to acquire one paying customer on a campaign.'}
             /></div>,
             dataIndex: 'cpa',
             key: 'cpa',
@@ -143,10 +148,10 @@ const ProductsList = ({products, onSearchChange, fetchParams, handlePaginationCh
             )
         },
         {
-            title: () => <div>Ad CVR<Tooltip getPopupContainer={trigger => trigger.parentNode}
-
-                                             description={'The Conversion Rate of a campaign is the percentage of people who clicked on an ad and then completed an action/purchase/conversion.'}
-            /></div>,
+            title: () => <div>Ad CVR
+                <Tooltip getPopupContainer={trigger => trigger.parentNode}
+                         description={'The Conversion Rate of a campaign is the percentage of people who clicked on an ad and then completed an action/purchase/conversion.'}
+                /></div>,
             dataIndex: 'conversion_rate',
             key: 'conversion_rate',
             width: '11em',
