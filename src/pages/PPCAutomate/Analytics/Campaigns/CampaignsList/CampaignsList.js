e9e1024f3cb490ccc644c3b83f1910d3bd1399f2
Romka <romka@MacBook-Pro-1.local>
2020-08-14 12:24:29 +0300

    added analytics page

--- src/pages/PPCAutomate/Analytics/Campaigns/CampaignsList/CampaignsList.js
+++ src/pages/PPCAutomate/Analytics/Campaigns/CampaignsList/CampaignsList.js
@@ -13,133 +13,133 @@ const CampaignsList = () => {
             dataIndex: 'active',
             key: 'active',
             width: '100px',
-            filter: true
+            sorter: true
         },
         {
             title: 'Campaigns',
             dataIndex: 'campaigns',
             key: 'campaigns',
             width: '200px',
-            filter: true
+            sorter: true
         },
         {
             title: 'Status',
             dataIndex: 'status',
             key: 'status',
             width: '150px',
-            filter: true
+            sorter: true
         },
         {
             title: 'Type',
             dataIndex: 'type',
             key: 'type',
             width: '200px',
-            filter: true
+            sorter: true
         },
         {
             title: 'Portfolio',
             dataIndex: 'portfolio',
             key: 'portfolio',
             width: '150px',
-            filter: true
+            sorter: true
         },
         {
             title: 'Campaign bidding strategy',
             dataIndex: 'bidding_strategy',
             key: 'bidding_strategy',
             width: '250px',
-            filter: true
+            sorter: true
         },
         {
             title: 'Start date',
             dataIndex: 'start_date',
             key: 'start_date',
             width: '150px',
-            filter: true
+            sorter: true
         },
         {
             title: 'Daily Budget',
             dataIndex: 'daily_budget',
             key: 'daily_budget',
             width: '150px',
-            filter: true
+            sorter: true
         },
         {
             title: 'Impressions',
             dataIndex: 'impressions',
             key: 'impressions',
             width: '150px',
-            filter: true
+            sorter: true
         },
         {
             title: 'Clicks',
             dataIndex: 'clicks',
             key: 'clicks',
             width: '150px',
-            filter: true
+            sorter: true
         },
         {
             title: 'CTR',
             dataIndex: 'ctr',
             key: 'ctr',
             width: '150px',
-            filter: true
+            sorter: true
         },
         {
             title: 'Spend',
             dataIndex: 'spend',
             key: 'spend',
             width: '150px',
-            filter: true
+            sorter: true
         },
         {
             title: 'CPC',
             dataIndex: 'cpc',
             key: 'cpc',
             width: '150px',
-            filter: true
+            sorter: true
         },
         {
             title: 'Orders',
             dataIndex: 'orders',
             key: 'orders',
             width: '150px',
-            filter: true
+            sorter: true
         },
         {
             title: 'Sales',
             dataIndex: 'sales',
             key: 'sales',
             width: '150px',
-            filter: true
+            sorter: true
         },
         {
             title: 'ACoS',
             dataIndex: 'acos',
             key: 'acos',
             width: '150px',
-            filter: true
+            sorter: true
         },
         {
             title: 'Conversion Rate',
             dataIndex: 'conversion_rate',
             key: 'conversion_rate',
             width: '150px',
-            filter: true
+            sorter: true
         },
         {
             title: 'CPA',
             dataIndex: 'cpa',
             key: 'cpa',
             width: '150px',
-            filter: true
+            sorter: true
         },
         {
             title: 'Profit',
             dataIndex: 'profit',
             key: 'profit',
             width: '150px',
-            filter: true
+            sorter: true
         },
     ];
 
