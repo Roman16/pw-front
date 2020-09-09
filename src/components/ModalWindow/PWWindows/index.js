e2f03e68f6ba81f119762c42cdd940fc8414934d
Romka <romka@MacBook-Pro-1.local>
2020-09-09 14:48:55 +0300

    added modal window about small spend

--- src/components/ModalWindow/PWWindows/index.js
+++ src/components/ModalWindow/PWWindows/index.js
@@ -1,41 +1,39 @@
-import React, {useEffect, useState} from "react";
-import StartFreeTrialWindow from "./StartFreeTrialWindow";
-import SubscriptionNotificationWindow from "./SubscriptionNotificationWindow";
-import LoadingAmazonAccount from "./LoadingAmazonAccountWindow";
-import ReportsChangesCountWindow from "./ReportsChangesCountWindow";
-import {useSelector} from "react-redux";
+import React, {useEffect, useState} from "react"
+import StartFreeTrialWindow from "./StartFreeTrialWindow"
+import SubscriptionNotificationWindow from "./SubscriptionNotificationWindow"
+import LoadingAmazonAccount from "./LoadingAmazonAccountWindow"
+import ReportsChangesCountWindow from "./ReportsChangesCountWindow"
+import {useSelector} from "react-redux"
 import SmallSpend from "./SmallSpend"
 
 const PWWindows = ({pathname}) => {
-    const [visibleWindow, setVisibleWindow] = useState(null);
+    const [visibleWindow, setVisibleWindow] = useState(null)
 
     const {user, productList, subscribedProduct} = useSelector(state => ({
         user: state.user,
         subscribedProduct: state.user.subscriptions[Object.keys(state.user.subscriptions)[0]],
         productList: state.products.productList || [],
-    }));
+    }))
 
     const closeWindowHandler = () => {
         setVisibleWindow(null)
-    };
+    }
 
     useEffect(() => {
         if (user.notifications.account_bootstrap.bootstrap_in_progress || (!subscribedProduct.has_access && subscribedProduct.has_pending_payment_tx)) {
-            setVisibleWindow('loadingAmazon');
-        }
-        // else if(subscribedProduct.quantity < 1000) {
-        //     setVisibleWindow('smallSpend');
-        // }
-        else if (user.user.free_trial_available) {
-            setVisibleWindow('freeTrial');
+            setVisibleWindow('loadingAmazon')
+        } else if (!subscribedProduct.eligible_for_subscription) {
+            setVisibleWindow('smallSpend')
+        } else if (user.user.free_trial_available) {
+            setVisibleWindow('freeTrial')
         } else if (!user.user.free_trial_available && !subscribedProduct.has_access && !subscribedProduct.has_pending_payment_tx) {
-            setVisibleWindow('expiredSubscription');
+            setVisibleWindow('expiredSubscription')
         } else if (user.notifications.ppc_optimization.count_from_last_login > 0 && subscribedProduct.has_access) {
-            setVisibleWindow('newReportsCount');
+            setVisibleWindow('newReportsCount')
         } else {
-            setVisibleWindow(null);
+            setVisibleWindow(null)
         }
-    }, [user]);
+    }, [user])
 
     return (
         <>
@@ -56,9 +54,9 @@ const PWWindows = ({pathname}) => {
                 onClose={closeWindowHandler}
             />}
 
-            {/*{pathname.includes('/ppc/') && <SmallSpend*/}
-            {/*    visible={visibleWindow === 'smallSpend'}*/}
-            {/*/>}*/}
+            {pathname.includes('/ppc/') && <SmallSpend
+                visible={visibleWindow === 'smallSpend'}
+            />}
 
             <ReportsChangesCountWindow
                 visible={visibleWindow === 'newReportsCount'}
@@ -67,6 +65,6 @@ const PWWindows = ({pathname}) => {
             />
         </>
     )
-};
+}
 
-export default PWWindows;
+export default PWWindows
