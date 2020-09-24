0444d88ce21b9b32f001f35b0d14a3f69319bb73
Romka <romka@MacBook-Pro-1.local>
2020-09-24 20:04:19 +0300

    fixed render start and stop optimization notification

--- src/reducers/products.reducer.js
+++ src/reducers/products.reducer.js
@@ -1,6 +1,8 @@
-import {productsConstants} from '../constans/actions.type';
+import {productsConstants} from '../constans/actions.type'
 
-const onlyOptimizationFromLocalStorage = localStorage.getItem('onlyOptimization') && JSON.parse(localStorage.getItem('onlyOptimization'));
+const onlyOptimizationFromLocalStorage = localStorage.getItem('onlyOptimization') && JSON.parse(localStorage.getItem('onlyOptimization')),
+    startNotificationFromLocalStorage = localStorage.getItem('startNotificationFromLocalStorage') && JSON.parse(localStorage.getItem('startNotificationFromLocalStorage')),
+    stopNotificationFromLocalStorage = localStorage.getItem('stopNotificationFromLocalStorage') && JSON.parse(localStorage.getItem('stopNotificationFromLocalStorage'))
 
 const initialState = {
     productList: [],
@@ -9,15 +11,15 @@ const initialState = {
     isFirstOptimization: true,
     isFirstChangesOptions: true,
     isFirstChangesStrategy: true,
-    dontShowStartNotificationAgain: false,
-    dontShowStopNotificationAgain: false,
+    dontShowStartNotificationAgain: !!startNotificationFromLocalStorage,
+    dontShowStopNotificationAgain: !!stopNotificationFromLocalStorage,
     selectedAll: false,
     onlyOptimization: onlyOptimizationFromLocalStorage && onlyOptimizationFromLocalStorage,
     onlyActiveOnAmazon: false,
     selectedProduct: {
         optimization_strategy: '',
     }
-};
+}
 
 export function products(state = initialState, action) {
     switch (action.type) {
@@ -28,7 +30,7 @@ export function products(state = initialState, action) {
                 totalSize: action.payload.fetching ? state.totalSize : action.payload.totalSize,
                 fetching: action.payload.fetching,
                 selectedProduct: action.payload.result && action.payload.result.length > 0 ? action.payload.result[0] : {}
-            };
+            }
 
         case productsConstants.UPDATE_SELECTED_PRODUCT:
             const newProductsList = state.productList.map(item => {
@@ -59,7 +61,7 @@ export function products(state = initialState, action) {
                 } else {
                     return item
                 }
-            });
+            })
             return {
                 ...state,
                 selectedProduct: {
@@ -69,7 +71,7 @@ export function products(state = initialState, action) {
                 },
                 productList: newProductsList,
                 isFirstOptimization: action.payload.status === 'RUNNING' ? false : state.isFirstOptimization
-            };
+            }
 
         case productsConstants.UPDATE_PRODUCT_OPTIONS:
             return {
@@ -78,64 +80,68 @@ export function products(state = initialState, action) {
                     ...state.defaultOptimizationOptions,
                     ...action.payload
                 }
-            };
+            }
 
         case productsConstants.CHANGE_OPTIONS:
             return {
                 ...state,
                 isFirstChangesOptions: false
-            };
+            }
 
         case productsConstants.CHANGE_STRATEGY:
             return {
                 ...state,
                 isFirstChangesStrategy: false
-            };
+            }
 
         case productsConstants.SELECT_PRODUCT:
             return {
                 ...state,
                 selectedAll: false,
                 selectedProduct: action.payload ? action.payload : state.selectedProduct
-            };
+            }
 
         case productsConstants.SELECT_ALL_PRODUCT:
             return {
                 ...state,
                 selectedAll: action.payload,
-            };
+            }
 
         case productsConstants.SET_FETCHING_STATE:
             return {
                 ...state,
                 fetching: action.payload,
-            };
+            }
 
         case productsConstants.SHOW_ONLY_OPTIMIZED:
-            localStorage.setItem('onlyOptimization', JSON.stringify(action.payload));
+            localStorage.setItem('onlyOptimization', JSON.stringify(action.payload))
 
             return {
                 ...state,
                 onlyOptimization: action.payload
-            };
+            }
 
         case productsConstants.SHOW_ONLY_ACTIVE:
             return {
                 ...state,
                 onlyActiveOnAmazon: action.payload
-            };
+            }
 
         case productsConstants.SWITCH_START_CONFIRM_WINDOW:
+            localStorage.setItem('startNotificationFromLocalStorage', JSON.stringify(action.payload))
+
             return {
                 ...state,
                 dontShowStartNotificationAgain: action.payload
-            };
+            }
 
         case productsConstants.SWITCH_STOP_CONFIRM_WINDOW:
+            localStorage.setItem('stopNotificationFromLocalStorage', JSON.stringify(action.payload))
+
             return {
                 ...state,
                 dontShowStopNotificationAgain: action.payload
-            };
+            }
 
         case productsConstants.CAMPAIGN_BUDGET:
             return {
@@ -145,16 +151,16 @@ export function products(state = initialState, action) {
                         item = {...item, ...action.payload}
                     }
 
-                    return item;
+                    return item
                 }),
                 selectedProduct: {
                     ...state.selectedProduct,
                     dailyBudget: action.payload.dailyBudget
                 }
-            };
+            }
 
         default:
-            return state;
+            return state
     }
 
 }
