16baa528bd97289af3623ef15bd68b0204b26ebf
Roman16 <roman.kostyanko@gmail.com>
2021-04-02 11:17:31 +0300

    added new partners logo

--- src/pages/LandingPages/Recognition/Recognition.js
+++ src/pages/LandingPages/Recognition/Recognition.js
@@ -13,10 +13,13 @@ import logo5 from '../../../assets/img/partners-logo/nozzle.png'
 import logo6 from '../../../assets/img/partners-logo/pingpong.png'
 import logo7 from '../../../assets/img/partners-logo/ylt-translations.png'
 import logo8 from '../../../assets/img/partners-logo/seller-fest.png'
+import logo9 from '../../../assets/img/partners-logo/payability.png'
+import logo10 from '../../../assets/img/partners-logo/sostocked.png'
+import logo11 from '../../../assets/img/partners-logo/accrueme.png'
 import {ContactForm} from "../Contact/Contact"
 
 const Recognition = () => {
-    const logoList = [logo0, logo1, logo2, logo3, logo4, logo5, logo6, logo7, logo8]
+    const logoList = [logo0, logo1, logo2, logo3, logo4, logo5, logo6, logo7, logo8, logo9, logo10, logo11]
 
     return (
         <div className="recognition  landing-page">
