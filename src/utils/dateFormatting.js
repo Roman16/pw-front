4d23e9cfef888a0a93081a25937e5dc5059c5928
Roman16 <roman.kostyanko@gmail.com>
2021-04-15 19:58:52 +0300

    fixed date format in update request

--- src/utils/dateFormatting.js
+++ src/utils/dateFormatting.js
@@ -2,5 +2,5 @@ import moment from "moment"
 import tz from 'moment-timezone'
 
 export const dateFormatting = (date) => {
-    return date ? moment(date).tz('America/Los_Angeles').toISOString() : undefined
+    return date ? moment(date).endOf('day').tz('America/Los_Angeles').format('YYYY-MM-DDTHH:mm:ss.000Z') : undefined
 }
