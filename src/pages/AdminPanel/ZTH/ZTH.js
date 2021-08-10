e0ee069be8983503aef3cf19ba2d5047d5116b75
Roman16 <rkostianko@intellias.com>
2021-08-10 22:25:10 +0300

    added delete cogs confirm window

--- src/pages/AdminPanel/ZTH/ZTH.js
+++ src/pages/AdminPanel/ZTH/ZTH.js
@@ -13,7 +13,7 @@ import CreateSemanticCore from "./CreateSemanticCore/CreateSemanticCore"
 
 const ZTH = () => {
     if (history.location.pathname === '/admin-panel/zth' || history.location.pathname === '/admin-panel/zth/') {
-        history.push('/admin-panel/zth/create')
+        history.push('/admin-panel/zth/convert')
     }
 
     const token = localStorage.getItem('adminToken') ? localStorage.getItem('adminToken') : localStorage.getItem('token')
@@ -23,25 +23,25 @@ const ZTH = () => {
             <TokenBlock/>
 
             <div className="tabs">
-                <NavLink to={'/admin-panel/zth/create'}>
-                    Create Semantic Core
-                </NavLink>
+                {/*<NavLink to={'/admin-panel/zth/create'}>*/}
+                {/*    Create Semantic Core*/}
+                {/*</NavLink>*/}
 
                 <NavLink to={'/admin-panel/zth/convert'}>
                     Convert Semantic Core
                 </NavLink>
 
-                <NavLink to={'/admin-panel/zth/creation-jobs'}>
-                    ZTH Creation Jobs
-                </NavLink>
+                {/*<NavLink to={'/admin-panel/zth/creation-jobs'}>*/}
+                {/*    ZTH Creation Jobs*/}
+                {/*</NavLink>*/}
 
                 <NavLink to={'/admin-panel/zth/jobs'}>
                     ZTH Upload Jobs
                 </NavLink>
 
-                <NavLink to={'/admin-panel/zth/templates'}>
-                    ZTH Templates
-                </NavLink>
+                {/*<NavLink to={'/admin-panel/zth/templates'}>*/}
+                {/*    ZTH Templates*/}
+                {/*</NavLink>*/}
 
                 <NavLink to={'/admin-panel/zth/asins-filtering'}>
                     ASINs Filtering
