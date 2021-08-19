47743a92561a799f43e14c9a9fd2e89f9cad3d9e
Roman16 <rkostianko@intellias.com>
2021-08-19 17:31:42 +0300

    PWD-1086 updated admin zth style

--- src/pages/Tableau/Tableau.js
+++ src/pages/Tableau/Tableau.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react"
 import './Tableau.less'
 import RouteLoader from "../../components/RouteLoader/RouteLoader"
 import {tableauServices} from "../../services/tableau.services"
+import TableauReport from 'tableau-react';
 
 const Tableau = () => {
     const [loading, setLoading] = useState(true),
@@ -14,9 +15,9 @@ const Tableau = () => {
             const res = await tableauServices.getToken()
             setTableauToken(res)
 
-            const url = await tableauServices.getUrl(res.result.urls.spsd)
+            // const url = await tableauServices.getUrl(res.result.urls.spsd)
 
-            console.log(url)
+            // console.log(url)
 
         } catch (e) {
             console.log(e)
@@ -24,6 +25,24 @@ const Tableau = () => {
 
         setLoading(false)
     }
+
+    const options = {
+        height: '100%',
+        width: '100%',
+        hideTabs: false,
+        // All other vizCreate options are supported here, too
+        // They are listed here: https://onlinehelp.tableau.com/current/api/js_api/en-us/JavaScriptAPI/js_api_ref.htm#ref_head_9
+    };
+
+    const filters = {
+        Colors: ['Blue', 'Red'],
+        Sizes: ['Small', 'Medium']
+    };
+
+    const parameters = {
+        Param1: 'Value',
+        Param2: 'Other Value'
+    };
 
 
     useEffect(() => {
@@ -34,16 +53,26 @@ const Tableau = () => {
     return (<div className={'tableau-page'}>
         {loading ? <RouteLoader/> :
             <>
-                <iframe
-                    src={`${tableauToken.result.urls.spsd}?:language=en-US&:display_count=n&:origin=viz_share_link`}
-                    width="100%"
-                    height="100%"
-                />
+                {/*<iframe*/}
+                {/*    src={`${tableauToken.result.urls.spsd}?:language=en-US&:display_count=n&:origin=viz_share_link`}*/}
+                {/*    width="100%"*/}
+                {/*    height="100%"*/}
+                {/*/>*/}
 
-                <iframe
-                    src={`http://tableau.profitwhales.com/views/ihorprofitwhales_agency/SPSD?:embed=y#2`}
-                    width="100%"
-                    height="100%"
+                {/*<iframe*/}
+                {/*    src={`http://tableau.profitwhales.com/views/ihorprofitwhales_agency/SPSD?:embed=y#2`}*/}
+                {/*    width="100%"*/}
+                {/*    height="100%"*/}
+                {/*/>*/}
+
+                <TableauReport
+                    url={tableauToken.result.urls.spsd}
+                    filters={filters}
+                    parameters={parameters}
+                    options={options} // vizCreate options
+                    // Overwrite default query params
+                    // defaults to '?:embed=yes&:comments=no&:toolbar=yes&:refresh=yes'
+                    query="?:embed=yes&:comments=no&:toolbar=yes&:refresh=yes"
                 />
             </>
         }
