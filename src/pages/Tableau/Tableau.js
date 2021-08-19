7c66dc6f4568d3f45c39f6caed4a328ea54418a7
Roman16 <rkostianko@intellias.com>
2021-08-19 13:16:42 +0300

    PWD-1086 updated admin zth style

--- src/pages/Tableau/Tableau.js
+++ src/pages/Tableau/Tableau.js
@@ -29,25 +29,12 @@ const Tableau = () => {
 
     return (<div className={'tableau-page'}>
         {loading ? <RouteLoader/> :
-
             <>
                 <iframe
-                    src={tableauToken.result.urls.spsd}
+                    src={`${tableauToken.result.urls.spsd}?:showVizHome=no&:embed=true`}
                     width="100%"
                     height="100%"
                 />
-
-                <script type='text/javascript' src='http://tableau.profitwhales.com/javascripts/api/viz_v1.js'></script>
-                <div className='tableauPlaceholder'>
-                    <object className='tableauViz' width='1920' height='851' style={{'display': 'none'}}>
-                        <param name='host_url' value='http%3A%2F%2Ftableau.profitwhales.com%2F'/>
-                        <param name='embed_code_version' value='3'/>
-                        <param name='site_root' value=''/>
-                        <param name='name' value='ihorprofitwhales_agency&#47;SPSD'/>
-                        <param name='tabs' value='no'/>
-                        <param name='toolbar' value='yes'/>
-                    </object>
-                </div>
             </>
         }
     </div>)
