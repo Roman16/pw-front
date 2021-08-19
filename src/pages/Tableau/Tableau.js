1ea64ecc5ffe7cd3f79c6a955fa3536aad1e86d7
Roman16 <rkostianko@intellias.com>
2021-08-19 14:17:10 +0300

    PWD-1086 updated admin zth style

--- src/pages/Tableau/Tableau.js
+++ src/pages/Tableau/Tableau.js
@@ -24,50 +24,27 @@ const Tableau = () => {
 
 
     useEffect(() => {
+
         getTableauToken()
     }, [])
 
     return (<div className={'tableau-page'}>
-        <script src="https://public.tableau.com/javascripts/api/viz_v1.js"></script>
-
         {loading ? <RouteLoader/> :
             <>
                 <iframe
-                    src={`${tableauToken.result.urls.spsd}`}
-                    width="100%"
-                    height="100%"
-                />
-                <iframe
-                    src={`${tableauToken.result.urls.fullview}?:embed=yes&:tabs=yes&:toolbar=yes`}
+                    src={`${tableauToken.result.urls.spsd}?:language=en-US&:display_count=n&:origin=viz_share_link`}
                     width="100%"
                     height="100%"
                 />
 
+                <iframe
+                    src={`https://public.tableau.com/views/Dashboardupd/Fullview?:language=en-US&:display_count=n&:origin=viz_share_link`}
+                    width="100%"
+                    height="100%"
+                />
             </>
         }
 
-        <div className='tableauPlaceholder' id='viz1629369153169'>
-            <noscript><a href='#'>
-                <img alt='7 days check '
-                     src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Da&#47;Dashboardupd&#47;Fullview&#47;1_rss.png'
-                /></a></noscript>
-            <object className='tableauViz' style={{'display': 'none'}}>
-                <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F'/>
-                <param name='embed_code_version' value='3'/>
-                <param name='site_root' value=''/>
-                <param name='name' value='Dashboardupd&#47;Fullview'/>
-                <param name='tabs' value='no'/>
-                <param name='toolbar' value='yes'/>
-                <param name='static_image'
-                       value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Da&#47;Dashboardupd&#47;Fullview&#47;1.png'/>
-                <param name='animate_transition' value='yes'/>
-                <param name='display_static_image' value='yes'/>
-                <param name='display_spinner' value='yes'/>
-                <param name='display_overlay' value='yes'/>
-                <param name='display_count' value='yes'/>
-                <param name='language' value='en-US'/>
-            </object>
-        </div>
 
     </div>)
 }
