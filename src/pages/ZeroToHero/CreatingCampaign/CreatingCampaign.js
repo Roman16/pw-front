c1f99ee9e8e8c6172dac14195851d6b1b0593a4c
Roman16 <rkostianko@intellias.com>
2021-09-29 22:14:54 +0300

    updated zth

--- src/pages/ZeroToHero/CreatingCampaign/CreatingCampaign.js
+++ src/pages/ZeroToHero/CreatingCampaign/CreatingCampaign.js
@@ -93,6 +93,7 @@ const CreatingCampaign = () => {
 
             <StepActions
                 currentStep={currentStep}
+                product={addedProducts[activeProductIndex]}
 
                 onChangeStep={setStepHandler}
             />
--- src/pages/ZeroToHero/CreatingCampaign/StepActions/StepActions.js
+++ src/pages/ZeroToHero/CreatingCampaign/StepActions/StepActions.js
@@ -1,12 +1,38 @@
 import React from "react"
 import './StepActions.less'
+import ProductItem from "../SelectProduct/ProductItem"
 
-const StepActions = ({currentStep, onChangeStep}) => {
-
+const StepActions = ({product, currentStep, onChangeStep}) => {
     return (
         <div className="zth-step-actions">
-            <button className="sds-btn white">Cancel</button>
-            <button className="sds-btn default" onClick={() => onChangeStep(currentStep + 1)}>Next</button>
+            {product && <ProductItem
+                key={product.id}
+                product={product}
+                // isOpened={product.id === openedProduct}
+                // isSelected={!!addedProducts.find(item => item.id === product.id)}
+                // isDisabled={!!addedProducts.find(item => item.id === product.id)}
+                // addedProducts={addedProducts}
+                // addedProducts={addedProducts}
+
+                // onSelect={selectProductHandler}
+                // onSelectVariation={selectVariationHandler}
+                // onOpenVariations={openVariationsListHandler}
+            />}
+
+            {currentStep > 0 && <button
+                className="sds-btn white"
+                onClick={() => onChangeStep(currentStep - 1)}
+            >
+                Back
+            </button>}
+
+            <button
+                className="sds-btn default"
+                disabled={!product}
+                onClick={() => onChangeStep(currentStep + 1)}
+            >
+                Next
+            </button>
         </div>
     )
 }
