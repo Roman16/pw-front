b94c7681ddd59a16bb5ac089d5a1d3e3afc9ecfb
Roman16 <roman.kostyanko@gmail.com>
2022-01-24 18:36:55 +0200

    added columns order window

--- src/pages/Analytics/components/TableList/ColumnsSelect.js
+++ src/pages/Analytics/components/TableList/ColumnsSelect.js
@@ -1,74 +1,105 @@
 import React, {useState} from "react"
-import {Checkbox, Input, Popover} from "antd"
+import {Checkbox, Input, Popover, Spin} from "antd"
 import {SVG} from "../../../../utils/icons"
+import ModalWindow from "../../../../components/ModalWindow/ModalWindow"
+import _ from "lodash"
+import {analyticsAvailableMetricsList} from "../../componentsV2/MainMetrics/metricsList"
+import {tabs} from "../../componentsV2/MainMetrics/MetricModal"
 
 const {Search} = Input
 
 const ColumnsSelect = ({columns, columnsBlackList, onChangeBlackList}) => {
+    const [visible, setVisible] = useState(false),
+        [activeTab, setActiveTab] = useState('all'),
+        [columnsState, setColumnsState] = useState(columns)
+
+
+    const changeTabHandler = (tab) => {
+        setActiveTab(tab)
+    }
+
+    const onSearch = (value) => {
+        // setColumnsState(columns.filter(column => column.title.toLowerCase().includes(value.toLowerCase())))
+    }
+
     return (
-        <Popover
-            trigger="click"
-            placement="bottomRight"
-            getPopupContainer={(node) => node.parentNode}
-            destroyTooltipOnHide={true}
-            overlayClassName={'popover-column-select'}
-            content={<ColumnList
-                columns={columns}
-                columnsBlackList={columnsBlackList}
-                onChangeBlackList={onChangeBlackList}
-            />}
-        >
-            <button className={'columns-select icon-btn'}>
+        <>
+            <button className={'columns-select icon-btn'} onClick={() => setVisible(true)}>
                 <i className={'btn icon'}>
                     <SVG id={'table-columns'}/>
                 </i>
                 columns
             </button>
-        </Popover>
+
+
+            <ModalWindow
+                footer={false}
+                className={'order-columns-window'}
+                destroyOnClose={true}
+                visible={visible}
+                onClose={() => setVisible(false)}
+            >
+                <div className="row">
+                    <ul className={'tabs'}>
+                        {tabs.map(i => <li
+                            onClick={() => changeTabHandler(i)}
+                            className={activeTab === i && 'active'}
+                        >
+                            {i}
+                        </li>)}
+                    </ul>
+
+                    <div className={'col'}>
+                        <h3>Available metrics</h3>
+                        <p>
+                            You can reorder metrics using Drag and Drop!
+                            Drag and Drop can only be used in "All" category and without any search filters
+                        </p>
+
+                        <div className="form-group">
+                            <Search
+                                className="search-field"
+                                placeholder={'Search'}
+                                onChange={e => onSearch(e.target.value)}
+                                data-intercom-target='search-field'
+                                suffix={<SVG id={'search'}/>}
+                            />
+
+                            <button className="btn transparent">
+                                Select all
+                            </button>
+                        </div>
+
+                        <div className="columns-list">
+                            {columnsState.map(column => (
+                                <Checkbox
+                                    disabled={column.locked}
+                                    checked={!columnsBlackList.find(key => key === column.key)}
+                                    // onChange={(e) => changeColumnHandler(e.target.checked, column.key)}
+                                >
+                                    {column.title}
+                                </Checkbox>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+
+                <div className="actions">
+                    <button className="btn transparent">
+                        Reset to default
+                    </button>
+
+                    <button className="btn white">
+                        Cancel
+                    </button>
+
+                    <button className="btn default">
+                        Apply
+                    </button>
+                </div>
+            </ModalWindow>
+        </>
     )
 }
-
-const ColumnList = ({columns, columnsBlackList, onChangeBlackList}) => {
-    const [columnsState, setColumnsState] = useState(columns)
-
-    const onSearch = (value) => {
-        setColumnsState(columns.filter(column => column.title.toLowerCase().includes(value.toLowerCase())))
-    }
-
-    const changeColumnHandler = (event, column) => {
-        if (event) {
-            onChangeBlackList(columnsBlackList.filter(item => item !== column))
-        } else {
-            onChangeBlackList([...columnsBlackList, column])
-        }
-    }
-
-    return (
-        <div>
-            <div className="form-group">
-                <Search
-                    className="search-field"
-                    placeholder={'Search'}
-                    onChange={e => onSearch(e.target.value)}
-                    data-intercom-target='search-field'
-                    suffix={<SVG id={'search'}/>}
-                />
-            </div>
-
-            <div className="columns-list">
-                {columnsState.map(column => (
-                    <Checkbox
-                        disabled={column.locked}
-                        checked={!columnsBlackList.find(key => key === column.key)}
-                        onChange={(e) => changeColumnHandler(e.target.checked, column.key)}
-                    >
-                        {column.title}
-                    </Checkbox>
-                ))}
-            </div>
-        </div>
-    )
-}
-
 
 export default React.memo(ColumnsSelect)
