ed490357cff68fa9a1ce5a15bbf3f96d86cccdbf
Roman16 <roman.kostyanko@gmail.com>
2022-02-15 16:11:29 +0200

    added 403 error handler

--- src/services/request.js
+++ src/services/request.js
@@ -73,6 +73,7 @@ const api = (method, url, data, type, abortToken, withDefaultUrl = true, showNot
                 }
             })
             .catch(error => {
+
                 if (!error.response && !('message' in error)) {
                     localStorage.removeItem('token')
                     localStorage.removeItem('adminToken')
@@ -107,7 +108,7 @@ const api = (method, url, data, type, abortToken, withDefaultUrl = true, showNot
                             }
                         } else if (error.response.status === 429) {
                             handlerErrors('This request is throttled, please try again later')
-                        } else if (error.response.data.message === 'Retry with' || (error.response.status === 402 && error.response.statusText === "Payment Required") || (error.response.status === 403 && error.response.statusText === "Forbidden")) {
+                        } else if (error.response.data.message === 'Retry with' || (error.response.status === 402 && error.response.statusText === "Payment Required") || (error.response.status === 403 && error.response.data.message === "Forbidden")) {
                         } else if (error.response.data.message !== 'Product not found') {
                             if (error.response.data) {
                                 handlerErrors(error.response.data.message ? error.response.data.message : error.response.data.error)
