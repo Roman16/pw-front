d7107a954fd8a731d3085da7a1fb6d62fab52572
Roman16 <roman.kostyanko@gmail.com>
2022-02-17 11:25:37 +0200

    added card form on registration page

--- src/pages/authentication/RegistrationPage/RegistrationPage.js
+++ src/pages/authentication/RegistrationPage/RegistrationPage.js
@@ -1,79 +1,29 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect} from 'react'
 import './RegistrationPage.less'
 import '../LoginPage/LoginPage.less'
 
 import PageDescription from "../LoginPage/PageDescription"
 import RegistrationForm from "./RegistrationForm"
-import Cookies from "js-cookie"
-import {userService} from "../../../services/user.services"
-import {history} from "../../../utils/history"
 import {useDispatch} from "react-redux"
 import {userActions} from "../../../actions/user.actions"
 import {seo} from "../../../utils/seo"
+import {Elements, StripeProvider} from "react-stripe-elements"
 
 const tapfiliateKey = process.env.REACT_APP_TAPFILIATE_KEY
 
-const RegistrationPage = (props) => {
-    const [user, setUser] = useState({
-            name: '',
-            last_name: '',
-            email: '',
-            password: '',
-            confirmPassword: '',
-        }),
-        [processing, setProcessing] = useState(false),
-        [failedFields, setFailedFields] = useState([])
+const stripeKey = process.env.REACT_APP_ENV === 'production'
+    ? process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY_LIVE
+    : process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY_TEST || 'pk_test_TYooMQauvdEDq54NiTphI7jx'
 
+const RegistrationPage = (props) => {
     const dispatch = useDispatch()
 
     const urlParams = new URLSearchParams(props.location.search)
 
-    const changeUserHandler = (value) => {
-        setFailedFields(failedFields.filter(i => i !== Object.keys(value)[0]))
-        setUser({...user, ...value})
+    const setUser = (user) => {
+        dispatch(userActions.setInformation({user: {email: user.email}}))
+
     }
-
-    const registrationHandler = async (e) => {
-        e.preventDefault()
-
-        if (Object.values(user).some(i => !i) || user.password.length < 6 || user.password !== user.confirmPassword) {
-            if (!user.name) setFailedFields(prevState => [...prevState, 'name'])
-            if (!user.last_name) setFailedFields(prevState => [...prevState, 'last_name'])
-            if (!user.email) setFailedFields(prevState => [...prevState, 'email'])
-            if (user.password.length < 6) setFailedFields(prevState => [...prevState, 'password'])
-            if (user.password !== user.confirmPassword) setFailedFields(prevState => [...prevState, 'confirmPassword'])
-        } else {
-            try {
-                setProcessing(true)
-
-                const ref = urlParams.get('ref') || localStorage.getItem('refId') || undefined
-                // const coupon = urlParams.get('coupon')
-
-                const res = await userService.regist({
-                    ...user,
-                    ...props.match.params.tag === 'from-agency' ? {is_agency_client: 1} : {},
-                    // ...Cookies.get('tap_vid') ? {tracking_id: Cookies.get('tap_vid')} : {},
-                    // ...coupon ? {coupon: coupon} : {},
-                    ...ref ? {referral_code: ref} : {},
-                    ...Cookies.get('_ga') && {'ga_cid': Cookies.get('_ga')}
-                })
-
-                dispatch(userActions.setInformation({user: {email: user.email}}))
-
-                localStorage.setItem('token', res.access_token)
-                localStorage.removeItem('refId')
-
-                seo({title: 'Sponsoreds'})
-
-                history.push('/confirm-email')
-            } catch (e) {
-                console.log(e)
-            }
-
-            setProcessing(false)
-        }
-    }
-
 
     useEffect(() => {
         window.tap('create', tapfiliateKey, {integration: "javascript"})
@@ -87,15 +37,14 @@ const RegistrationPage = (props) => {
     return (
         <div className="auth-page registration-page">
             <div className="container">
-                <RegistrationForm
-                    user={user}
-                    failedFields={failedFields}
-                    processing={processing}
-
-                    onChange={changeUserHandler}
-                    onSubmit={registrationHandler}
-                />
-
+                <StripeProvider apiKey={stripeKey}>
+                    <Elements>
+                        <RegistrationForm
+                            setUser={setUser}
+                            {...props}
+                        />
+                    </Elements>
+                </StripeProvider>
                 <PageDescription/>
             </div>
 
