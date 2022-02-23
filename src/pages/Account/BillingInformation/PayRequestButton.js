9515b6c7814cd4e745fc015260d476159d325400
Roman16 <roman.kostyanko@gmail.com>
2022-02-23 11:40:14 +0200

    added payment request buttons

--- src/pages/Account/BillingInformation/PayRequestButton.js
+++ src/pages/Account/BillingInformation/PayRequestButton.js
@@ -4,7 +4,7 @@ import {injectStripe, PaymentRequestButtonElement} from "react-stripe-elements"
 class PaymentRequestForm extends React.Component {
     constructor(props) {
         super(props);
-
+//"tok_1KWHBNJzUVfwvcYwSc4UFOq6"
         // For full documentation of the available paymentRequest options, see:
         // https://stripe.com/docs/stripe.js#the-payment-request-object
         const paymentRequest = props.stripe.paymentRequest({
@@ -12,7 +12,7 @@ class PaymentRequestForm extends React.Component {
             currency: 'usd',
             total: {
                 label: 'Demo total',
-                amount: 1000,
+                amount: 100,
             },
         });
 
@@ -20,6 +20,42 @@ class PaymentRequestForm extends React.Component {
             console.log('Received Stripe token: ', token);
             console.log('Received customer information: ', data);
             complete('success');
+        });
+
+        paymentRequest.on('paymentmethod', async (ev) => {
+            // Confirm the PaymentIntent without handling potential next actions (yet).
+            console.log(ev)
+
+            // const {paymentIntent, error: confirmError} = await stripe.confirmCardPayment(
+            //     clientSecret,
+            //     {payment_method: ev.paymentMethod.id},
+            //     {handleActions: false}
+            // );
+
+            // if (confirmError) {
+            //     // Report to the browser that the payment failed, prompting it to
+            //     // re-show the payment interface, or show an error message and close
+            //     // the payment interface.
+            //     ev.complete('fail');
+            // } else {
+            //     // Report to the browser that the confirmation was successful, prompting
+            //     // it to close the browser payment method collection interface.
+            //     ev.complete('success');
+            //     // Check if the PaymentIntent requires any actions and if so let Stripe.js
+            //     // handle the flow. If using an API version older than "2019-02-11"
+            //     // instead check for: `paymentIntent.status === "requires_source_action"`.
+            //     if (paymentIntent.status === "requires_action") {
+            //         // Let Stripe.js handle the rest of the payment flow.
+            //         const {error} = await stripe.confirmCardPayment(clientSecret);
+            //         if (error) {
+            //             // The payment failed -- ask your customer for a new payment method.
+            //         } else {
+            //             // The payment has succeeded.
+            //         }
+            //     } else {
+            //         // The payment has succeeded.
+            //     }
+            // }
         });
 
         paymentRequest.canMakePayment().then((result) => {
