23de1e6f6610f0f0ebb6cb5d11a8d20edb58be8c
Roman16 <roman.kostyanko@gmail.com>
2022-04-18 13:04:13 +0300

    updated subscriptions page

--- src/pages/Account/Subscriptions/PageHeader.js
+++ src/pages/Account/Subscriptions/PageHeader.js
@@ -1,8 +1,7 @@
 import React from "react"
 
 export const PageHeader = ({user}) => {
-
     return (<section className="page-header">
-        <p>Select subscription for <b>{}</b> (seller id: <b>{user.default_accounts.amazon_mws.seller_id}</b>)</p>
+        <p>Select subscription for <b>{user.default_accounts.amazon_mws.account_name}</b> (seller id: <b>{user.default_accounts.amazon_mws.seller_id}</b>)</p>
     </section>)
 }
