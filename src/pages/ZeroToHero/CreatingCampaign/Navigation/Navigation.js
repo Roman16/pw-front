6f178c8fc0bc41b9b0eb1da3585515b974481b2a
Roman16 <roman.kostyanko@gmail.com>
2022-06-06 12:04:20 +0300

    changed main colors

--- src/pages/ZeroToHero/CreatingCampaign/Navigation/Navigation.js
+++ src/pages/ZeroToHero/CreatingCampaign/Navigation/Navigation.js
@@ -26,7 +26,7 @@ const Navigation = ({currentStep, openedSteps, onChangeStep}) => {
 
 export const CheckSmall = () => <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
     <mask id="mask0" maskUnits="userSpaceOnUse" x="0" y="0" width="14" height="14">
-        <rect x="0.5" y="0.5" width="13" height="13" fill="#791313" stroke="#312C51"/>
+        <rect x="0.5" y="0.5" width="13" height="13" fill="#791313" stroke="#353A3E"/>
     </mask>
     <g>
         <path d="M1 7.92593L5 12L13 2" stroke="white" stroke-width="2"/>
