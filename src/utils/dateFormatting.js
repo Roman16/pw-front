c1ad3be7e19fb26056c1f707f7cc280e6483f08d
Roman16 <roman.kostyanko@gmail.com>
2022-11-01 18:57:17 +0200

    fixed datepiker in analytics

--- src/utils/dateFormatting.js
+++ src/utils/dateFormatting.js
@@ -1,7 +1,8 @@
 import moment from 'moment-timezone'
+import {activeTimezone} from "../pages"
 
 export const dateFormatting = (date) => {
-    return date ? moment(date).endOf('day').format('YYYY-MM-DDTHH:mm:ss.000Z') : undefined
+    return date ? `${moment(date).format('YYYY-MM-DD')}${moment().tz(activeTimezone).format('T23:59:59.999Z')}` : undefined
 }
 export const dateRequestFormat = (date) => {
     return date ? encodeURIComponent(`${moment(date).format('YYYY-MM-DDT23:59:59.999Z')}`) : undefined
