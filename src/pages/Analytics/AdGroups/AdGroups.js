63cfd32931e342bb8ab39f8b2ebd38c2a74d57b9
Roman16 <roman.kostyanko@gmail.com>
2022-11-29 11:54:33 +0200

    added portfolio changes request

--- src/pages/Analytics/AdGroups/AdGroups.js
+++ src/pages/Analytics/AdGroups/AdGroups.js
@@ -35,10 +35,10 @@ const AdGroups = () => {
                 rowKey={'adGroupId'}
 
                 columns={columnList(setStateHandler, selectedCampaign)}
-                moreActions={<OpenCreateWindowButton title={'Add Ad Group'} window={'adGroup'}/>}
+                // moreActions={<OpenCreateWindowButton title={'Add Ad Group'} window={'adGroup'}/>}
             />
 
-            <CreateAdGroupWindow/>
+            {/*<CreateAdGroupWindow/>*/}
         </div>
     )
 }
