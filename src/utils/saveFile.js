a001dcad48357f3919c0f2a288e815fa1f704e7d
Andrew Bulat <andrii.bulat@gmail.com>
2023-05-30 20:08:44 +0100

    Revert "changed download bulksheets function"
    This reverts commit 432eee588c48c0f859ac8548fc02f9e3d57d660a.

--- src/utils/saveFile.js
+++ src/utils/saveFile.js
@@ -2,11 +2,11 @@ import _ from "lodash"
 import XLSX from "xlsx"
 
 export const saveFile = (file, type) => {
-    // if (type === 'csv') {
-    //     saveAsCsv(file)
-    // } else if (type === 'xls' || type === 'xlsx') {
-    saveAsExcel(file, type)
-    // }
+    if (type === 'csv') {
+        saveAsCsv(file)
+    } else if (type === 'xls' || type === 'xlsx') {
+        saveAsExcel(file, type)
+    }
 }
 
 export const saveInputParameters = (objs, name='input-parameters') => {
@@ -46,37 +46,30 @@ export const saveSpreadsheet = (res) => {
     XLSX.writeFile(workbook, workbookFilename)
 }
 
-const saveAsExcel = (bulksheet, type) => {
+
+const saveAsExcel = (doc, type) => {
+    const staticData = {
+        fieldSeparator: '|',
+        rowSeparator: '\n',
+    }
+
+    const data = doc.document
+        .split(staticData.rowSeparator)
+        .map(x =>
+            x.split(staticData.fieldSeparator)
+                .map(y => _.isString(y) && y.length === 0 ? null : y)
+        )
+
+    const sheet = XLSX.utils.aoa_to_sheet(data)
     const workbook = XLSX.utils.book_new()
-
-    bulksheet.allSheets.forEach(sheet => {
-        const sheetDataAsArrays = sheet
-            .csvContent
-            .split(sheet.rowSeparator)
-            .map(row =>
-                row.split(sheet.fieldSeparator)
-                    .map(column => _.isString(column) && column.length === 0 ? null : column),
-            )
-
-        const worksheet = XLSX.utils.aoa_to_sheet(sheetDataAsArrays)
-
-        // xlsx sheet name can't be longer than 31 characters
-        const truncatedSheetName = sheet.name.substring(0, 31)
-        XLSX.utils.book_append_sheet(workbook, worksheet, truncatedSheetName)
-    })
-
-    saveWorkbook(workbook, bulksheet.name, type)
+    XLSX.utils.book_append_sheet(workbook, sheet, doc.documentName.substr(0, 31))
+    saveWorkbook(workbook, doc.documentName, type)
 }
 
 const saveAsCsv = (doc) => {
-    let textFile = ''
-
-    doc.allSheets.forEach(sheet => {
-        textFile = textFile + _makeTextFile(sheet.csvContent)
-    })
-
+    const textFile = _makeTextFile(doc.document)
     const link = document.createElement('a')
-    link.download = `${doc.name}.csv`
+    link.download = `${doc.documentName}.csv`
     link.href = textFile
     const el = document.querySelector('body')
     el.appendChild(link)
@@ -93,4 +86,3 @@ const _makeTextFile = (text) => {
 const saveWorkbook = (workbook, name, extension) => {
     XLSX.writeFile(workbook, `${name}.${extension}`)
 }
-
